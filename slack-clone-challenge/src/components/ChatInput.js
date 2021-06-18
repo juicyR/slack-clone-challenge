e42dc64369d2af4ad04582500f412105ad1be3bf
juicyR <matovureginald@gmail.com>
2021-06-18 09:37:22 +0300

    Text area update

--- slack-clone-challenge/src/components/ChatInput.js
+++ slack-clone-challenge/src/components/ChatInput.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import styled from 'styled-components';
+import SendIcon from '@material-ui/icons/Send';
 
 function ChatInput() {
     return (
         <Container>
-            ChatInput
+            <InputContainer>
+                <form>
+                    <input type="text" placeholder="Type Message Here..." />
+                    <SendButton>
+                        <SendIcon />
+                    </SendButton>
+                </form>
+            </InputContainer>
         </Container>
     )
 }
@@ -12,5 +20,31 @@ function ChatInput() {
 export default ChatInput
 
 const Container = styled.div`
-    background: blue;
+    padding: 0px 20px 24px 20px;
+`
+
+const InputContainer = styled.div`
+    border: 1px solid #8d8d8e;
+    border-radius: 4px;
+    padding: 0.5em;
+
+    form {
+        height: 42px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    input {
+        width: 100%;
+        border: none;
+    }
+
+    input:focus {
+        outline: none;
+    }
+`
+
+const SendButton = styled.div`
+    margin-left: 8px;
 `
